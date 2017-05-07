7220d43d86b3cd3aea304878ef552eef4ae60e7e
VishnevskyViktorov <vromchela@yandex.ru>
2017-05-07 08:32:33 +0700

    Init project

---
+++ VishnevskyViktorov.Nsudotnet.TicTacToe/VishnevskyViktorov.Nsudotnet.TicTacToe/ShellBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using VishnevskyViktorov.Nsudotnet.TicTacToe.ViewModels;
+
+namespace VishnevskyViktorov.Nsudotnet.TicTacToe {
+    public class ShellBootstrapper : BootstrapperBase {
+        public ShellBootstrapper()
+            : base(true) {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e) {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ VishnevskyViktorov.Nsudotnet.TicTacToe/VishnevskyViktorov.Nsudotnet.TicTacToe/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using System.Windows;
+
+namespace VishnevskyViktorov.Nsudotnet.TicTacToe.ViewModels {
+    public class MainViewModel : PropertyChangedBase {
+        public MainViewModel() {
+            HelloWorld = "Hello World";
+        }
+
+        private string _helloworld;
+
+        public string HelloWorld {
+            get {
+                return _helloworld;
+            }
+            set {
+                _helloworld = value;
+                NotifyOfPropertyChange(() => HelloWorld);
+            }
+        }
+
+        //Don't use this code in production
+        public void ShowMessage() {
+            HelloWorld = "AA";
+            MessageBox.Show("Test Caliburn.Micro");
+        }
+    }
+}
